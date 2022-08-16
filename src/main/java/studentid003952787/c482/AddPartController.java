cad5f65a0c779a92ba1723540d8b5b86718a74ef
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-08-16 12:26:29 -0600

    added cancel logic on add part form

--- src/main/java/studentid003952787/c482/AddPartController.java
+++ src/main/java/studentid003952787/c482/AddPartController.java
@@ -20,4 +20,12 @@ public class AddPartController {
         stage.show();
     }
 
+    public void cancel(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainView.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
