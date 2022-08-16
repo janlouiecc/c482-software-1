a3e27708ce6db82447588213198c99aa9f9ca1cf
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-08-16 13:03:55 -0600

    added modify part function to main form

--- src/main/java/studentid003952787/c482/MainController.java
+++ src/main/java/studentid003952787/c482/MainController.java
@@ -21,6 +21,14 @@ public class MainController {
         stage.show();
     }
 
+    public void modifyPart(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ModifyPartView.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void exit() {
         Platform.exit();
         System.exit(0);
