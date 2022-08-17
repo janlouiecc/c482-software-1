9246b35c919dd935923c5b3ae0ceffb759274f72
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-08-17 11:07:21 -0600

    added inhouse and outsourced subclasses

---
+++ src/main/java/wgu/softwareproject/InHouse.java
@@ -0,0 +1,10 @@
+package wgu.softwareproject;
+
+public class InHouse extends Part{
+
+
+
+    public InHouse(int id, String name, double price, int stock, int min, int max) {
+        super(id, name, price, stock, min, max);
+    }
+}
