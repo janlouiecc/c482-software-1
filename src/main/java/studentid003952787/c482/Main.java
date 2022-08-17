da458d4c2106794365f0fffefbe9506e0a90c51a
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-08-17 09:09:41 -0600

    added part class given, and also created product class

--- src/main/java/studentid003952787/c482/Main.java
+++ src/main/java/studentid003952787/c482/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainView.fxml")));
---
+++ src/main/java/studentid003952787/c482/Product.java
@@ -0,0 +1,72 @@
+package studentid003952787.c482;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Product {
+
+    private static final ObservableList<Part> associatedParts = FXCollections.observableArrayList();
+    private int productId;
+    private String productName;
+    private double productPrice;
+    private int productStock;
+    private int productMin;
+    private int productMax;
+
+    public Product(int productId, String productName, double productPrice, int productStock, int productMin, int productMax) {
+        setProductId(productId);
+        setProductName(productName);
+        setProductPrice(productPrice);
+        setProductStock(productStock);
+        setProductMin(productMin);
+        setProductMax(productMax);
+    }
+
+    public void setProductId(int productId) {
+        this.productId =  productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public void setProductStock(int productStock) {
+        this.productStock = productStock;
+    }
+
+    public void setProductMin(int productMin) {
+        this.productMin = productMin;
+    }
+
+    public void setProductMax(int productMax) {
+        this.productMax = productMax;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public int getProductStock() {
+        return productStock;
+    }
+
+    public int getProductMin() {
+        return productMin;
+    }
+
+    public int getProductMax() {
+        return productMax;
+    }
+}
