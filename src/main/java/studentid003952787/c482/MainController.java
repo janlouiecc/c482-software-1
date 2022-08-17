8a6e4710607f0ddca2acb2d7f86198eb9ccee879
janlouiecc <70475659+janlouiecc@users.noreply.github.com>
2022-08-16 21:54:13 -0600

    added add product form info and updated main form

--- src/main/java/studentid003952787/c482/MainController.java
+++ src/main/java/studentid003952787/c482/MainController.java
@@ -29,6 +29,14 @@ public class MainController {
         stage.show();
     }
 
+    public void addProduct(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AddProductView.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void exit() {
         Platform.exit();
         System.exit(0);
